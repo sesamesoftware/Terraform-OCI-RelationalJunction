7096b19764ef086c39d75468fdfc5fa9f07105e5
williamdubberley <william.dubberley@sesamesoftware.com>
2021-06-29 13:36:17 -0500

    tfvars

--- main.tf
+++ main.tf
@@ -10,10 +10,7 @@ Local Variables
 locals {
   public_subnet_id  = var.vcn_use_existing ? var.subnet_public_existing : module.vcn.public-subnetid
   private_subnet_id = var.vcn_use_existing ? var.subnet_private_existing : module.vcn.private-subnetid
-  ssh_public_key    = file("${var.ssh_public_key_path}")
-  private_key       = file("${var.ssh_private_key_path}")
-
-}
+  }
 /* Virtual Cloud Network  */
 module "vcn" {
   source                     = "./.terraform/modules/source/networking"
@@ -37,11 +34,12 @@ module "vcn" {
 
 }
 
- 
+
 //Autonomous Data Wearhouse
 
 module "adw" {
-  source                                       = "./.terraform/modules/source/Databases/ADW"
+  source                                       = "./.terraform/modules/source/Databases/ADb"
+  count                                        = var.adw_enabled == true ? 1 : 0
   autonomous_database_admin_password           = var.autonomous_database_admin_password
   compartment_ocid                             = var.compartment_ocid
   autonomous_database_cpu_core_count           = var.autonomous_database_cpu_core_count
@@ -49,23 +47,57 @@ module "adw" {
   autonomous_database_db_name                  = var.autonomous_database_db_name
   autonomous_database_db_version               = var.autonomous_database_db_version
   autonomous_database_data_safe_status         = var.autonomous_database_data_safe_status
-  autonomous_database_db_workload              = var.autonomous_database_db_workload
+  autonomous_database_db_workload              = "DW"
   autonomous_database_display_name             = var.autonomous_database_display_name
   autonomous_database_is_auto_scaling_enabled  = var.autonomous_database_is_auto_scaling_enabled
   autonomous_database_license_model            = var.autonomous_database_license_model
   adw_enabled                                  = var.adw_enabled
   walletPath                                   = "${var.walletPath}/${var.autonomous_database_db_name}.zip"
 }
-output "ADW_URL" {
-  description = "url to connect to ADW"
-  value       = var.adw_enabled ? module.adw.ADW_URL : null
+
+//Autonomous Transactin Procesing
+
+module "atp" {
+  source                                       = "./.terraform/modules/source/Databases/ADb"
+  count                                        = var.atp_enabled == true ? 1 : 0
+  autonomous_database_admin_password           = var.autonomous_database_admin_password
+  compartment_ocid                             = var.compartment_ocid
+  autonomous_database_cpu_core_count           = var.autonomous_database_cpu_core_count
+  autonomous_database_data_storage_size_in_tbs = var.autonomous_database_data_storage_size_in_tbs
+  autonomous_database_db_name                  = var.autonomous_database_db_name
+  autonomous_database_db_version               = var.autonomous_database_db_version
+  autonomous_database_data_safe_status         = var.autonomous_database_data_safe_status
+  autonomous_database_db_workload              = "OLTP"
+  autonomous_database_display_name             = var.autonomous_database_display_name
+  autonomous_database_is_auto_scaling_enabled  = var.autonomous_database_is_auto_scaling_enabled
+  autonomous_database_license_model            = var.autonomous_database_license_model
+  adw_enabled                                  = var.adw_enabled
+  walletPath                                   = "${var.walletPath}/${var.autonomous_database_db_name}.zip"
 }
+//Autonomous JSON Database
 
-
+module "ajd" {
+  source                                       = "./.terraform/modules/source/Databases/ADb"
+  count                                        = var.ajd_enabled == true ? 1 : 0
+  autonomous_database_admin_password           = var.autonomous_database_admin_password
+  compartment_ocid                             = var.compartment_ocid
+  autonomous_database_cpu_core_count           = var.autonomous_database_cpu_core_count
+  autonomous_database_data_storage_size_in_tbs = var.autonomous_database_data_storage_size_in_tbs
+  autonomous_database_db_name                  = var.autonomous_database_db_name
+  autonomous_database_db_version               = var.autonomous_database_db_version
+  autonomous_database_data_safe_status         = var.autonomous_database_data_safe_status
+  autonomous_database_db_workload              = "AJD"
+  autonomous_database_display_name             = var.autonomous_database_display_name
+  autonomous_database_is_auto_scaling_enabled  = var.autonomous_database_is_auto_scaling_enabled
+  autonomous_database_license_model            = var.autonomous_database_license_model
+  adw_enabled                                  = var.adw_enabled
+  walletPath                                   = "${var.walletPath}/${var.autonomous_database_db_name}.zip"
+}
 // Database
-/*
+
 module "DBAS" {
   source                  = "./.terraform/modules/source/Databases/dbas"
+  count                   = var.dbas_enabled == true ? 1 : 0
   depends_on              = [module.vcn]
   compartment_ocid        = var.compartment_ocid
   tenancy_ocid            = var.tenancy_ocid
@@ -80,45 +112,40 @@ module "DBAS" {
   database_edition        = var.database_edition
   database_nodecount      = var.database_nodecount
   subnet_id               = local.public_subnet_id
-  ssh_public_keys         = local.ssh_public_key
+  ssh_public_keys         = var.ssh_public_key
   domain                  = module.vcn.Public_Subnet_Dns
   database_display_name   = var.database_display_name
   database_hostname       = var.database_hostname
 }
-output "dbConnectionString" {
-  description = "url to connect to Relational Junction"
-  value       = module.DBAS.dbConnectionString
-}
-*/
+
 
 //Relational Junction 
 
 module "rj" {
   source           = "./.terraform/modules/source/relationalJunction"
+  count            = var.rj_enabled == true ? 1 : 0
   compartment_ocid = var.compartment_ocid
   depends_on       = [module.vcn, module.adw]
   rj_shape         = var.rj_shape
   rj_instance_name = var.rj_instance_name
   tags             = var.tags
   subnet_id        = local.public_subnet_id
-  ssh_public_key   = local.ssh_public_key
-  ssh_private_key  = local.private_key
+  ssh_public_key   = var.ssh_public_key
+  ssh_private_key  = var.ssh_private_key
   walletPath       = "${path.module}/"
   walletName       = "${var.autonomous_database_db_name}.zip"
   dbname           = var.autonomous_database_db_name
   region           = var.region
   tenancy_ocid     = var.tenancy_ocid
 }
-output "RJ_URL" {
-  description = "url to connect to Relational Junction"
-  value       = module.rj.RJ_URL
-}
+
 
 
 //Oracle Analytics Cloud
-/* 
+
 module "oac" {
   source             = "./.terraform/modules/source/oac"
+  count              = var.oac_enabled == true ? 1 : 0
   oac_enabled        = var.oac_enabled
   oac_capacity_type  = var.oac_capacity_type
   oac_capacity_value = var.oac_capacity_value
@@ -130,9 +157,12 @@ module "oac" {
   oac_description    = var.oac_description
 }
 
-output "OAC_URL" {
-  description = "url to connect to OAC"
-  value       = var.oac_enabled ? module.oac.OAC_URL : null
-}
 
-*/
+module "OBS" {
+  source             = "./.terraform/modules/source/ObjectStorage"
+  count              = var.obs_enabled == true ? 1 : 0
+  obs_name           = var.obs_name
+  namespace          = var.namespace
+  compartment_ocid   = var.compartment_ocid
+  bucket_access_type = var.bucket_access_type
+}
